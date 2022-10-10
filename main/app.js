e0ac6de33409b297eaf30c7b33dc479622833c8f
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-11 03:32:09 +0600

    js almost done

--- main/app.js
+++ main/app.js
@@ -1,8 +1,32 @@
-function clear(){
+const myResult = document.getElementById("myResult")
 
+
+/* clear options */
+function deleteMe(){
+    myResult.value ='';
+}
+
+/* one by one delete options */
+function delte1(){
+    myResult.value = myResult.value.slice(0,-1);
 }
 
 
+/* Values ​​are displayed one by one in the input file */
 function display(newValue){
-    document.getElementById("myResult").value += newValue;
+    myResult.value += newValue;
+}
+
+
+
+/* this is the mathmatical function */
+function answer(){
+   
+   try {
+    myResult.value = eval(myResult.value);
+    }
+   catch{
+    alert('Invalid')
+    myResult.value = "";
+   } 
 }
